5899f487e1e238acbc4624917f4a79c033d927d8
szokeasaurusrex <szokeasaurusrex@users.noreply.github.com>
2015-12-30 15:59:43 -0500

    rearanged parts of vote.php

--- voter/vote.php
+++ voter/vote.php
@@ -1,15 +1,4 @@
 <?php
-  function getVotes($ballot) {
-    $sql = "SELECT * FROM votes";
-    $result = $conn->query($sql);
-    $data = $result->fetch_assoc();
-    $votes_string = $data[$ballot];
-    return intval($votes_string);
-  }
-  function vote($ballot, $votes) {
-    $sql = "UPDATE votes SET " + $ballot + " = " + (string)$votes;
-    return $conn->query($sql);
-  }
   $servername = "localhost";
   $username = "voting";
   $password = "";
@@ -22,7 +11,17 @@
   if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
   }
-
+  function getVotes($ballot) {
+    $sql = "SELECT * FROM votes";
+    $result = $conn->query($sql);
+    $data = $result->fetch_assoc();
+    $votes_string = $data[$ballot];
+    return intval($votes_string);
+  }
+  function vote($ballot, $votes) {
+    $sql = "UPDATE votes SET " + $ballot + " = " + (string)$votes;
+    return $conn->query($sql);
+  }
   $ballot = $_REQUEST["ballot"];
   $votes_to_insert = getVotes($ballot) + 1;
   $success = vote($ballot, $votes_to_insert);
