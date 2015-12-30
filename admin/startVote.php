d733257452ae284177e30558e39da2287f59b065
szokeasaurusrex <szokeasaurusrex@users.noreply.github.com>
2015-12-30 11:57:37 -0500

    Start vote php code added

---
+++ admin/startVote.php
@@ -0,0 +1,23 @@
+<?php
+  $servername = "localhost";
+  $username = "voting";
+  $password = "";
+  $dbName = "voting"
+
+  // Create connection
+  $conn = new mysqli($servername, $username, $password, $dbName);
+
+  // Check connection
+  if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+  }
+
+  $sql = "UPDATE votes SET yes = 0, no = 0";
+  if ($conn->query($sql) === TRUE) {
+    echo "start";
+  } else {
+    echo "Fail: " . $conn->error;
+  }
+
+  $conn->close();
+?>
--- admin/voting.js
+++ admin/voting.js
@@ -1,21 +1,12 @@
 app.controller("voting", function ($scope) {
   $scope.voting = false;
-  $scope.votingStatus = "";
   $scope.btn_action = "Start";
   $scope.yes_votes = 0;
   $scope.no_votes = 0;
-  $scope.updateVotingStatus = function(response) {
-    if (response == "start") {
-      $scope.votingStatus = "Voting in progress";
-    } else {
-      $scope.votingStatus = response;
-    }
-  }
   $scope.btnClick = function () {
     if ($scope.voting == false) {
       $scope.voting = true;
       $scope.btn_action = "Stop";
-      $.get("startVote.php", null, $scope.updateVotingStatus, "text");
     } else {
       $scope.voting = false;
       $scope.btn_action = "Start";
