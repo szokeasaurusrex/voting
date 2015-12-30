868b13091dcac43e8af0cb76d4b5a74ac7a090e9
szokeasaurusrex <szokeasaurusrex@users.noreply.github.com>
2015-12-30 13:41:09 -0500

    added debug code

--- admin/voting.js
+++ admin/voting.js
@@ -4,19 +4,18 @@ app.controller("voting", function ($scope) {
   $scope.btn_action = "Start";
   $scope.yes_votes = 0;
   $scope.no_votes = 0;
-  $scope.updateVotingStatus = function(response) {
-    console.log("function")
-    if (response == "start") {
-      $scope.voting_status = "Voting in progress";
-    } else {
-      $scope.voting_status = response;
-    }
-  }
   $scope.btnClick = function () {
     if ($scope.voting == false) {
       $scope.voting = true;
       $scope.btn_action = "Stop";
-      $.get("startVote.php", null, $scope.updateVotingStatus, "text");
+      $.get("startVote.php", null, function(response) {
+        console.log("function")
+        if (response == "start") {
+          $scope.voting_status = "Voting in progress";
+        } else {
+          $scope.voting_status = response;
+        }
+      }, "text");
     } else {
       $scope.voting = false;
       $scope.btn_action = "Start";
