16b50e90c62049a77816639f04bf087d5c64b08c
szokeasaurusrex <szokeasaurusrex@users.noreply.github.com>
2015-12-30 15:48:39 -0500

    Draft voter interface

---
+++ voter/vote.php
@@ -0,0 +1,34 @@
+<?php
+  function getVotes($ballot) {
+    $sql = "SELECT * FROM votes";
+    $result = $conn->query($sql);
+    $data = $result->fetch_assoc();
+    $votes_string = $data[$ballot];
+    return intval($votes_string);
+  }
+  function vote($ballot, $votes) {
+    $sql = "UPDATE votes SET " + $ballot + " = " + (string)$votes;
+    return $conn->query($sql);
+  }
+  $servername = "localhost";
+  $username = "voting";
+  $password = "";
+  $dbName = "voting";
+
+  // Create connection
+  $conn = new mysqli($servername, $username, $password, $dbName);
+
+  // Check connection
+  if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+  }
+
+  $ballot = $_REQUEST["ballot"];
+  $votes_to_insert = getVotes($ballot) + 1;
+  $success = vote($ballot, $votes_to_insert);
+  if ($success === TRUE) {
+    echo "success";
+  } else {
+    echo "fail";
+  }
+?>
---
+++ voter/voter.js
@@ -0,0 +1,15 @@
+app.controller("voter", function($scope) {
+  $scope.vote = function (ballot) {
+    data = {
+      ballot: "";
+    };
+    data.ballot = ballot;
+    $.get("vote.php", data, function(response) {
+      if (response == "success") {
+        alert("You're vote was successfully processed.");
+      } else {
+        alert("Error: You're vote could not be processed correctly.")
+      }
+    }, "text")
+  };
+});
