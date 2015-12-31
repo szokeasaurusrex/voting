8250655b2e4171904e9dd308cf356fbddc3f7ba6
szokeasaurusrex <szokeasaurusrex@users.noreply.github.com>
2015-12-30 22:27:02 -0500

    Voting during processing prohibited

--- voter/voter.js
+++ voter/voter.js
@@ -1,15 +1,20 @@
 app.controller("voter", function($scope) {
+  $scope.processing = false;
   $scope.vote = function (ballot) {
-    data = {
-      ballot: ""
-    };
-    data.ballot = ballot;
-    $.get("vote.php", data, function(response) {
-      if (response == "success") {
-        alert("You're vote was successfully processed.");
-      } else {
-        alert("Error: You're vote could not be processed correctly.");
-      }
-    }, "text");
+    if ($scope.processing === false) {
+      $scope.processing = true;
+      data = {
+        ballot: ""
+      };
+      data.ballot = ballot;
+      $.get("vote.php", data, function(response) {
+        $scope.processing = false;
+        if (response == "success") {
+          alert("You're vote was successfully processed.");
+        } else {
+          alert("Error: You're vote could not be processed correctly.");
+        }
+      }, "text");
+    }
   };
 });
