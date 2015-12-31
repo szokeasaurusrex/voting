c4b3834468eac208f9a377007d5ff5814f88e144
szokeasaurusrex <szokeasaurusrex@users.noreply.github.com>
2015-12-31 15:52:57 -0500

    more debug code

--- admin/voting.js
+++ admin/voting.js
@@ -22,6 +22,7 @@ app.controller("voting", function ($scope) {
       $scope.voting = false;
       $scope.btn_action = "Start";
       $.get("getResults.php", null, function(result) {
+        console.log(result);
         var votes = result.votes;
         $scope.voters = result.voters;
         $scope.yes_votes = votes.yes;
